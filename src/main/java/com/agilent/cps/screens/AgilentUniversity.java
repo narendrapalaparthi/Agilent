9a448b7fb84ca7ef561ed03a2add0963ddf7bddd
narendra.r.palaparthi@gmail.com <narendra.r.palaparthi@gmail.com>
2020-07-21 16:52:11 +0530

    Demo changes

--- src/main/java/com/agilent/cps/screens/AgilentUniversity.java
+++ src/main/java/com/agilent/cps/screens/AgilentUniversity.java
@@ -4,7 +4,7 @@ public class AgilentUniversity extends BaseScreen{
 	
 	public void accessElearningCourse(String course) {
 		seleniumActions.clickLink("Access online e-learning");
-		seleniumActions.clickLinkWithParialText("Agilent University ePass - Unlimited Self-paced Online Course Access");
+		seleniumActions.clickLinkWithParialText(course);
 	}
 
 }
